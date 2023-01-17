823d757205978516a8f1c12a694747cd5b2282c8
HwangKiyong <121090747+HwangKiyong@users.noreply.github.com>
2023-01-17 14:44:11 +0900

    Add files via upload

---
+++ 02.sql
@@ -0,0 +1,90 @@
+-- where
+select employee_id, last_name, department_id
+from employees
+where department_id = 50;
+--boolean 타입의 where을 쓰는거다. true만 조회하게된다.
+
+-- 과제: 176번 사원의 사번, 이름, 부서번호를 조회하라.
+select employee_id, last_name, department_id
+from employees
+where employee_id = 176;
+
+select employee_id, last_name, department_id
+from employees
+where last_name = 'Whalen';
+/*statement에서는 대소문자 구별안한다. 
+data에서는 대소문자를 구별해야한다.*/
+
+select employee_id, last_name, hire_date
+from employees
+where hire_date = '2008/02/06';
+
+select last_name, salary
+from employees
+where salary <= 3000;
+
+-- 과제: 월 $12,000 이상 버는 사원들의 이름, 월급을 조회하라.
+select last_name, salary
+from employees
+where salary >= 12000;
+
+select last_name, job_id
+from employees
+where job_id != 'IT_PROG';
+
+select last_name, salary
+from employees
+where salary between 2500 and 3500;
+--between A and B 면 A이상 B이하이다.
+
+select last_name
+from employees
+where last_name between 'King' and 'Smith';
+
+-- 과제: 'King' 사원의 first_name, last_name, 직업, 월급을 조회하라.
+select first_name, last_name, job_id, salary
+from employees
+where last_name = 'King';
+
+select last_name, hire_date
+from employees
+where hire_date between '2002/01/01' and '2002/12/31';
+
+select employee_id, last_name, manager_id
+from employees
+where manager_id in (100, 101, 201);
+-- in 은 점선이다. between은 실선이다.
+
+select employee_id, last_name, manager_id
+from employees
+where manager_id = 100 or
+    manager_id = 101 or
+    manager_id = 201;
+--or는 한가지만 맞아도 true로 출력이된다.
+
+select employee_id, last_name
+from employees
+where last_name in ('Hartstein', 'Vargars');
+
+select last_name, hire_date
+from employees
+where hire_date in ('2003/06/17', '2005/09/21');
+
+select last_name
+from employees
+where last_name like 'S%';
+-- like 다음에는 char타입만 올수있다. like는 비슷하다의 표현이다.
+
+select last_name
+from employees
+where last_name like '%r';
+
+--과제: 이름에 s가 포함된 사원들의 이름을 조회하라.
+select last_name
+from employees
+where last_name like '%s%';
+
+--과제: 2005년에 입사한 사원들의 이름, 입사일을 조회하라.
+select last_name, hire_date
+from employees
+where hire_date like '2005%';
