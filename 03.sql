e4585cee396765e203642e4f0a29afc1d2a6cd15
HwangKiyong <121090747+HwangKiyong@users.noreply.github.com>
2023-01-17 17:54:11 +0900

    Add files via upload

---
+++ 03.sql
@@ -0,0 +1,14 @@
+--single function
+desc dual
+select * from dual;
+
+select lower('SQL Course')
+from dual;
+
+select upper('SQL Course')
+from dual;
+--upper 대문자로 다 바꾼다.
+
+select initcap('SQL Course')
+from dual;
+--init 은 문자의 앞글자만 대문자로 바꾼다.
